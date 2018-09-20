0ac203201e77ee0ff9062225f8f430e42c85b594
killbozer <35951632+killbozer@users.noreply.github.com>
2018-09-20 20:29:17 +0930

    Creating basic items from json file

--- scripts.js
+++ scripts.js
@@ -1,7 +1,27 @@
 
+function showMembers(jsobObj) {
+    var items = jsonObj['appMembers'];
+
+    for (var index = 0; index < items.length; i++) {
+
+        var target = document.getElementById("result");
+        var contain = document.createElement('div');
+        var pic = document.createElement('div');
+        var info = document.createElement('div');
+        var text = document.createElement('p');
+
+        text.textContent = 'Bio: ' + items[i].bio + 'Location: ' + items[i].loc;
+
+        target.appendChild(contain);
+        contain.appendChild(pic);
+        contain.appendChild(info);
+        info.appendChild(text);
+    }
+
+}
 
 
-var element = document.getElementById("result");
+
 
 
 
@@ -16,6 +36,6 @@ request.send();
 request.onload = function () {
 
     var members = request.response;
-    console.log(members)
+    showMembers(members);
 
 }
